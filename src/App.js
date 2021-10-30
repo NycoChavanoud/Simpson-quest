0d1d5a9f6d13eb07dd72ba2e46256b16fcc385b1
NycoChavanoud <nicolas.chavanoud@gmail.com>
2021-10-30 14:42:59 +0200

    exercice de la quete terminé

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import axios from "axios";
+import Simpson from "./Simpson";
+
+const simpsonModel = {
+  quote:
+    "Facts are meaningless. You could use facts to prove anything that's even remotely true.",
+  character: "Homer Simpson",
+  image:
+    "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939",
+  characterDirection: "Right",
+};
 
 function App() {
+  const [simpson, setSimpson] = useState("");
+
+  const getQuote = () => {
+    axios
+      .get("https://simpsons-quotes-api.herokuapp.com/quotes")
+      .then((res) => res.data)
+      .then((data) => setSimpson(data[0]));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Simpson simpson={simpson} />
+      <button type="button" onClick={getQuote}>
+        New Quote
+      </button>
+    </>
   );
 }
 
